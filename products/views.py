14dfc9468c342ce3250a06c3d05f0b8be74a57d7
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-14 21:03:01 +0430

    Add view and url for Product app.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from products.models import Product
+
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    context_object_name = 'product'
