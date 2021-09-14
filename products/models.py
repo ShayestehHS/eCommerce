5c199c0051a9caffb7cb3874c76199945c3801f6
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-14 10:28:30 +0430

    Create model of Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,46 @@
+import os
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+def get_image_upload_path(instance, filename):
+    return os.path.join('Products', 'images', instance.title, filename)
+
+
+class ProductQuerySet(models.query.QuerySet):
+    def featured(self):
+        self.filter(is_featured=True)
+
+    def active(self):
+        self.filter(is_active=True)
+
+
+class ProductModelManager(models.Manager):
+    def featured(self):
+        self.get_queryset().filter(is_featured=True)
+
+    def active(self):
+        self.get_queryset().filter(is_active=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120,
+                             help_text="Maximum length is 120 character.")
+    slug = models.SlugField(blank=True, unique=True,
+                            help_text="This field is not required.")
+    image = models.ImageField(upload_to=get_image_upload_path, )
+    price = models.DecimalField(max_digits=5, decimal_places=2,
+                                help_text="Maximum price is 999.99")
+    description = models.TextField()
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = ProductModelManager()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
