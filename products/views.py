04b89b09e6d66c93e69065efaefddbf05b8275ba
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-16 10:50:52 +0430

    Optimize get_queryset of ProductListView.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from products.models import Product
@@ -7,6 +6,13 @@ from products.models import Product
 class ProductListView(ListView):
     queryset = Product.objects.all()
     context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = self.queryset \
+            .prefetch_related('tagged_items') \
+            .defer('is_featured', 'is_active', 'timestamp')
+
+        return queryset
 
 
 class ProductDetailView(DetailView):
