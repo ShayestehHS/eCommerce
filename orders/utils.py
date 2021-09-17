f21b0417f711144597bb30bba19b1c43a82be36b
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-17 13:31:52 +0430

    Add unique_order_id_generator.

--- orders/utils.py
+++ orders/utils.py
@@ -1,7 +1,8 @@
 from random import choice
-from string import ascii_lowercase,digits
+from string import ascii_lowercase, digits
 
-def random_string_generator(size=10,chars=ascii_lowercase + digits):
+
+def random_string_generator(size=10, chars=ascii_lowercase + digits):
     return ''.join(choice(chars) for _ in range(size))
 
 
