21fb1ba3b34fb5f455c9f36daaba450d788f26c2
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-17 13:30:47 +0430

    Add unique_order_id_generator.

---
+++ orders/utils.py
@@ -0,0 +1,14 @@
+from random import choice
+from string import ascii_lowercase,digits
+
+def random_string_generator(size=10,chars=ascii_lowercase + digits):
+    return ''.join(choice(chars) for _ in range(size))
+
+
+def unique_order_id_generator(instance, size):
+    while True:
+        new_order_id = random_string_generator(size)
+        Klass = instance.__class__
+        qs_exists = Klass.objects.filter(order_id=new_order_id).exists()
+        if not qs_exists:
+            return new_order_id.upper()
