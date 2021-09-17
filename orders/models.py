7674dad33f13b616dead5d3b84c917d9886202a7
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-17 13:31:07 +0430

    Add unique_order_id_generator.

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from carts.models import Cart
+from orders.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -11,8 +12,7 @@ ORDER_STATUS_CHOICES = (
 
 
 class Order(models.Model):
-    order_id = models.CharField(max_length=10, editable=False)
-
+    order_id = models.CharField(max_length=10, editable=False, blank=True)
     cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE)
     status = models.CharField(max_length=8, choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(max_digits=7, decimal_places=2,
@@ -20,3 +20,11 @@ class Order(models.Model):
                                          help_text='Maximum total is 99999.99')
     total = models.DecimalField(max_digits=7, decimal_places=2, default=0,
                                 help_text='Maximum total is 99999.99')
+
+    def save(self, *args, **kwargs):
+        if not self.order_id:
+            self.order_id = unique_order_id_generator(self, 10)
+        super(Order, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_id
