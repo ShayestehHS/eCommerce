1b2878b20480d265aee101c41c80b3aa85b3441c
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-18 16:28:08 +0430

    Add tax to calculation process of cart_total.

--- carts/signals.py
+++ carts/signals.py
@@ -1,13 +1,15 @@
 from django.db.models.signals import m2m_changed, post_save, pre_save
 from django.dispatch import receiver
 
+from decimal import Decimal
+
 from carts.models import Cart
 from orders.models import Order
 
 
 @receiver(m2m_changed, sender=Cart.products.through)
 def m2m_changed_cart_receiver(sender, instance, action, **kwargs):
-    if 'post_' in action:  # post_save post_remove post_clear
+    if 'post_' in action:  # post_save || post_remove || post_clear
         subtotal = 0
         for product in instance.products.all():
             subtotal += product.price
@@ -19,11 +21,11 @@ def m2m_changed_cart_receiver(sender, instance, action, **kwargs):
 
 @receiver(pre_save, sender=Cart)
 def cart_pre_save(sender, instance, **kwargs):
-    instance.total = instance.subtotal + 10 if instance.subtotal != 0 else 0
+    if instance.subtotal is not 0:
+        instance.total = Decimal(instance.subtotal) * Decimal(1.08)  # Tax %8
 
 
 @receiver(post_save, sender=Cart)
 def cart_post_save(sender, instance, created, **kwargs):
-    print(created)
     if created:
         Order.objects.create(cart=instance)
