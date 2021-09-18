60ed7e71154256fb6da01084d4168536e117e475
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-18 13:55:46 +0430

    Add default to 'status' field.

--- orders/models.py
+++ orders/models.py
@@ -14,9 +14,8 @@ ORDER_STATUS_CHOICES = (
 class Order(models.Model):
     order_id = models.CharField(max_length=10, editable=False, blank=True)
     cart = models.OneToOneField(to=Cart, on_delete=models.CASCADE)
-    status = models.CharField(max_length=8, choices=ORDER_STATUS_CHOICES)
-    shipping_total = models.DecimalField(max_digits=7, decimal_places=2,
-                                         default=0,
+    status = models.CharField(max_length=8, choices=ORDER_STATUS_CHOICES, default='created')
+    shipping_total = models.DecimalField(max_digits=7, decimal_places=2, default=0,
                                          help_text='Maximum total is 99999.99')
     total = models.DecimalField(max_digits=7, decimal_places=2, default=0,
                                 help_text='Maximum total is 99999.99')
