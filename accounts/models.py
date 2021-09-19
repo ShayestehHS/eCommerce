48455819db98cf4a2bd2c975678b7f5f8bbb491f
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-19 16:13:17 +0430

    Remove last_name, full_name from User model.

--- accounts/models.py
+++ accounts/models.py
@@ -27,8 +27,10 @@ class CustomUserManager(BaseUserManager):
 
 class User(AbstractUser):
     username = None
+    first_name = None
+    last_name = None
     email = models.EmailField(max_length=127, unique=True)
-    full_name = models.CharField(max_length=127, blank=True, null=True)
+    full_name = models.CharField(max_length=127)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
