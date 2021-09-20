fce143dac320c73c9954b5782113e0a34abd77c7
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-20 08:28:24 +0430

    Unregister Group.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from accounts.models import User
+
+admin.site.unregister(Group)
+
 
 @admin.register(User)
 class UserAdminModel(admin.ModelAdmin):
