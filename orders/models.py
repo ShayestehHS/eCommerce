5ad2fde69f9ddd3baa60045c780b90a3dea8bb03
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-21 11:18:42 +0430

    Add address ForeignKey to Order.

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from carts.models import Cart
+from billing.models import Address
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -14,6 +15,12 @@ class Order(models.Model):
     cart = models.OneToOneField(to=Cart, on_delete=models.CASCADE)
     order_id = models.CharField(max_length=10, editable=False, blank=True)
     status = models.CharField(max_length=8, choices=ORDER_STATUS_CHOICES, default='created')
+    address_shipping = models.ForeignKey(Address, on_delete=models.CASCADE,
+                                         null=True, blank=True,
+                                         related_name='address_shipping')
+    address_billing = models.ForeignKey(Address, on_delete=models.CASCADE,
+                                        null=True, blank=True,
+                                        related_name='address_billing')
 
     def __str__(self):
         return self.order_id
