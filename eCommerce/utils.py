4a77461e53c825e537afe61811e428e0b11ffcdf
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-21 11:25:30 +0430

    Some change in MessageMixin

--- eCommerce/utils.py
+++ eCommerce/utils.py
@@ -3,6 +3,9 @@ from django.contrib import messages
 
 
 def is_valid_url(request, url, require_https=False):
+    if url is None:
+        return False
+
     allowed_hosts = request.get_host()
     if not is_safe_url(url, allowed_hosts, require_https):
         messages.error(request, "You can't hack me😎😎")
@@ -14,11 +17,13 @@ def is_valid_url(request, url, require_https=False):
 class MessageMixin:
     message = ''
     message_level = messages.SUCCESS  # Default tag
+    message_delete_other = False
 
-    def send_message(self, request, default_messages=True):
-        if default_messages is False:
+    def send_message(self, request):
+        if self.message_delete_other is True:
             """ Delete stored messages """
             storage = messages.get_messages(request)
             storage.used = True
 
-        messages.add_message(request=request, level=self.message_level,message=self.message)
+        messages.add_message(request=request, level=self.message_level,
+                             message=self.message)
--- eCommerce/views.py
+++ eCommerce/views.py
@@ -7,10 +7,11 @@ from eCommerce.utils import MessageMixin
 class HomeTemplateView(TemplateView, MessageMixin):
     template_name = 'eCommerce/home.html'
     message = 'WELCOME'
+    message_delete_other = True
 
     def get_context_data(self, **kwargs):
         super(HomeTemplateView, self).get_context_data()
-        self.send_message(self.request, default_messages=False)
+        self.send_message(self.request)
 
 
 class SearchProductListView(ListView):
