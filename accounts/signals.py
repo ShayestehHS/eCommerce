f4ebbc052311a276e24269bb8a153ef5063c7985
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-21 11:22:25 +0430

    Change post_save signal.

--- accounts/signals.py
+++ accounts/signals.py
@@ -10,8 +10,6 @@ User = settings.AUTH_USER_MODEL
 
 
 @receiver(post_save, sender=User)
-def user_post_save(sender, instance, created, **kwargs):
+def user_post_save(instance, created, *args, **kwargs):
     if created:
-        cart = Cart.objects.create(user=instance)
-        BillingProfile.objects.create(user=instance)
-        Order.objects.create(cart=cart)
+        Cart.objects.create(user=instance)
