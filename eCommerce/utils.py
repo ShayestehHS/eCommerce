922580a4cb5d2cd0c989005808460cd6ec204947
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-23 10:27:43 +0330

    Move update_session from cart to eCommerce

--- eCommerce/utils.py
+++ eCommerce/utils.py
@@ -2,18 +2,6 @@ from django.utils.http import is_safe_url
 from django.contrib import messages
 
 
-def is_valid_url(request, url, require_https=False):
-    if url is None:
-        return False
-
-    allowed_hosts = request.get_host()
-    if not is_safe_url(url, allowed_hosts, require_https):
-        messages.error(request, "You can't hack me😎😎")
-        return False
-
-    return True
-
-
 class MessageMixin:
     message = ''
     message_level = messages.SUCCESS  # Default tag
@@ -27,3 +15,20 @@ class MessageMixin:
 
         messages.add_message(request=request, level=self.message_level,
                              message=self.message)
+
+
+def is_valid_url(request, url, require_https=False):
+    if not url:
+        return False
+
+    allowed_hosts = request.get_host()
+    if not is_safe_url(url, allowed_hosts, require_https):
+        messages.error(request, "You can't hack me 😎😎")
+        return False
+
+    return True
+
+
+def update_session(request, cart, is_new=False):
+    request.session['cart_id'] = cart.id
+    request.session['cart_items'] = cart.products.count() if not is_new else 0
