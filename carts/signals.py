2bc7372177f56756f6e2fa5207bed773c64299ea
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-23 10:19:16 +0330

    Update the m2m signal.

--- carts/signals.py
+++ carts/signals.py
@@ -8,12 +8,23 @@ from orders.models import Order
 @receiver(m2m_changed, sender=Cart.products.through)
 def m2m_changed_cart_receiver(instance, action, *args, **kwargs):
     if 'post_' in action:  # post_save || post_remove || post_clear
-        shipping_total = 0
+        subtotal = 0
         for product in instance.products.all():
-            shipping_total += product.price
+            subtotal += product.price
 
-        instance.shipping_total = shipping_total
-        instance.save(update_fields=['shipping_total'])
+        instance.subtotal = subtotal
+        instance.total = instance.calculate_total(subtotal)
+        instance.save(update_fields=['subtotal', 'total'])
+
+        user_id = instance.user_id
+        if user_id:
+            order, is_new = Order.objects.get_or_create(user_id=user_id, cart=instance)
+            instance.set_cart_to_order(order)
+
+
+@receiver(post_save, sender=Cart)
+def cart_post_save(instance, created, *args, **kwargs):
+    if created and instance.user_id:
         try:
             order = Order.objects.get(user_id=instance.user_id)
             instance.set_cart_to_order(order)
