83e54a8fe5483c585c552072cdaffc4dd3ba356a
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-28 12:20:56 +0330

    Create ObjectViewed and register it.

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from analytics.models import ObjectViewed
+
+
+@admin.register(ObjectViewed)
+class ObjectViewedModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class ObjectViewed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    ip_address = models.GenericIPAddressField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.content_object} viewed on {self.timestamp}'
+
+    class Meta:
+        ordering = ['-timestamp']
+        verbose_name = 'Object viewed'
+        verbose_name_plural = 'Objects viewed'
