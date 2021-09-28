1061bd93d17d00d48ce6d9e75a052b5464462fa8
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-09-28 12:01:50 +0330

    Set Timeout for ajax call

--- static/static_dirs/js/register.js
+++ static/static_dirs/js/register.js
@@ -1,3 +1,12 @@
+function Timeout(fn, interval) {
+    const id = setTimeout(fn, interval);
+    this.cleared = false;
+    this.clear = function () {
+        this.cleared = true;
+        clearTimeout(id);
+    };
+}
+
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -19,32 +28,41 @@ conPassword.on('input', function () {
 
 $(document).ready(function () {
     const emailField = $('#id_email');
+    var ajaxTimeOut = null;
     emailField.on('input', function () {
-        // Check input is null or not
-        if (!$(this).val() === "") {
+        const emailValue = emailField.val();
+        // Check input is not null
+        if (!emailValue === "") {
             emailField.css("box-shadow", "none");
             return null;
         }
 
-        // Check input is valid email address or not
-        if (!validateEmail($(this).val())) {
+        // Check input is valid email address
+        if (!validateEmail(emailValue)) {
             emailField.css("box-shadow", "none");
             return null;
         }
-        alert(emailField.val())
-        // Send AJAX
-        $.ajax({
-            type: 'GET',
-            url: $(this).closest('form').data('ajax-url'),
-            data: {'email': emailField.val(),},
-            success: function (data) {
-                alert(data.email_exists)
-                if (data.email_exists === true) {
-                    emailField.css('box-shadow', '0px 14px 12px -5px #44FF36');
-                } else if (data.email_exists === false) {
-                    emailField.css('box-shadow', '0px 14px 12px -5px #ff3333');
-                }
+        // Check time is out
+        if (ajaxTimeOut != null) {
+            if (!ajaxTimeOut.cleared) {
+                return null
             }
-        })
+        }
+
+        ajaxTimeOut = new Timeout(function () {
+            $.ajax({
+                type: 'GET',
+                url: emailField.closest('form').data('ajax-url'),
+                data: {'email': emailValue,},
+                success: function (data) {
+                    if (data.email_exists === true) {
+                        emailField.css('box-shadow', '0px 14px 12px -5px #44FF36');
+                    } else if (data.email_exists === false) {
+                        emailField.css('box-shadow', '0px 14px 12px -5px #ff3333');
+                    }
+                }
+            });
+            ajaxTimeOut.clear();
+        }, 3000);
     })
 })
