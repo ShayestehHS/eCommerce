e2450037e146f4fd1e78a625c997e8052515a5bc
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-10-17 00:21:23 +0330

    Create signal and model for UserSession and register it

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from analytics.models import ObjectViewed
+from analytics.models import ObjectViewed, UserSession
 
 
 @admin.register(ObjectViewed)
 class ObjectViewedModelAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+
+
+@admin.register(UserSession)
+class UserModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'active')
+    list_display = ['__str__', 'active']
+    actions = ['end_session_action']
+
+    @admin.action(description="Inactive and end session")
+    def end_session_action(self, request, queryset):
+        objs = []
+        for session in queryset:
+            session.end_session(bulk_update=True)
+            objs.append(session)
+        UserSession.objects.bulk_update(objs, ['active', 'ended'])
--- analytics/models.py
+++ analytics/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.sessions.models import Session
 
 User = settings.AUTH_USER_MODEL
 
@@ -21,3 +22,30 @@ class ObjectViewed(models.Model):
         ordering = ['-timestamp']
         verbose_name = 'Object viewed'
         verbose_name_plural = 'Objects viewed'
+
+
+class UserSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    ip_address = models.CharField(max_length=127)
+    session_key = models.CharField(max_length=32)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    ended = models.BooleanField(default=False)
+
+    def end_session(self, bulk_update=False):
+        session_key = self.session_key
+        try:
+            Session.objects.get(pk=session_key).delete()
+        except Session.DoesNotExist:
+            print(f"ERROR: Session DoesNotExist (session key is {session_key})")
+            bulk_update = False  # Force to update the instance
+
+        self.ended = True
+        self.active = False
+        if bulk_update is False:
+            self.save(update_fields=['ended', 'active'])
+
+        return self.ended
+
+    def __str__(self):
+        return str(self.user)
