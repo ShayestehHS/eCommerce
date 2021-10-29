6263b1c3f7498011f32d101502e3207219c8f965
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-10-29 18:02:26 +0330

    Minor change.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,5 +3,6 @@
 set -e
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 
 uwsgi --module eCommerce.wsgi --socket :8000 --master --enable-threads
--- scripts/run.sh
+++ scripts/run.sh
@@ -7,9 +7,8 @@ ls -la /vol/web
 
 whoami
 
-#python manage.py wait_for_db
+python manage.py wait_for_db
 python manage.py collectstatic --noinput
-python manage.py makemigrations
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module eCommerce.wsgi
