0299e74e185f5efafdd44cab29b4a5c5da93479c
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-10-29 20:21:10 +0330

    Minor change.

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,8 +3,17 @@
 set -e
 
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+
+ls
+
 cd var
 ls
 cd ..
+
+cd val/local
+ls
+cd ..
+
+find . -type d -name "static"
 #tree -d .
 nginx -g 'daemon off;'
