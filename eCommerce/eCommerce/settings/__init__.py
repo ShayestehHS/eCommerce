4848f578743a6a22ccdbc9cde5e8ab6e3c0ff62e
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-10-30 18:00:35 +0330

    Minor change in colorizing logs

--- eCommerce/eCommerce/settings/__init__.py
+++ eCommerce/eCommerce/settings/__init__.py
@@ -8,17 +8,14 @@ FAIL = '\033[91m'
 
 if setting_env == 'LOCAL':
     from eCommerce.settings.local import *
-
     print(OKCYAN + "-- Local setting imported --" + ENDC)
 
 elif setting_env == 'PRODUCTION':
     from eCommerce.settings.production import *
-
     print(OKCYAN + "-- Production setting imported --" + ENDC)
 
 elif setting_env == "TEST":
     from eCommerce.settings.test import *
-
     print(OKCYAN + "-- Test setting imported --" + ENDC)
 
 else:
