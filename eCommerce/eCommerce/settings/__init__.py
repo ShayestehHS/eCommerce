b1229c5d1f92c8621bdce8ee19a28faf5bb12e60
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-10-30 17:46:45 +0330

    Change color of status color in settings/__init__.py

--- eCommerce/eCommerce/settings/__init__.py
+++ eCommerce/eCommerce/settings/__init__.py
@@ -4,23 +4,27 @@ ENDC = '\033[0m'
 WARNING = '\033[93m'
 OKGREEN = '\033[92m'
 OKCYAN = '\033[96m'
+FAIL = '\033[91m'
 
 if setting_env == 'LOCAL':
     from eCommerce.settings.local import *
+
     print(OKCYAN + "-- Local setting imported --" + ENDC)
 
 elif setting_env == 'PRODUCTION':
     from eCommerce.settings.production import *
+
     print(OKCYAN + "-- Production setting imported --" + ENDC)
 
 elif setting_env == "TEST":
     from eCommerce.settings.test import *
+
     print(OKCYAN + "-- Test setting imported --" + ENDC)
 
 else:
     raise Exception(OKCYAN + '-- Invalid setting_env --' + ENDC)
 
 if DEBUG is True:
-    print(WARNING + "-- Debug mode is: ON --" + ENDC)
+    print(WARNING + "-- Debug mode is:" + FAIL + "ON --" + ENDC)
 else:
     print(OKGREEN + "-- Debug mode is: OFF --" + ENDC)
