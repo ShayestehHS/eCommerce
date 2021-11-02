ee83d40560de0fe0423b927892fca4e85833d3d6
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-11-02 12:20:23 +0330

    Delete unused file.

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-
-gunicorn eCommerce.wsgi:application --bind 0.0.0.0:8000
